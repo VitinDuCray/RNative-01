2bb16bba7603bf2c4a1b66e1077ca7dfdc72e920
Victor Hugo <161778672+VitinDuCray@users.noreply.github.com>
2024-12-14 03:16:05 -0300

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#3a0d6e',
+        color: 'white',
+      }}>
+      <Text>O mais forte é aquele que reconhece as suas fraquezas.</Text>
+    </View>
+  );
+};
+
+export default YourApp;
